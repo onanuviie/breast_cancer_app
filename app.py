16ecccb7ccbfda08ab2244a5a73cfe0e3355ce08
onanuviie <nuviesilvet@gmail.com>
2025-05-21 14:19:50 +0100

    deployyyy

--- app.py
+++ app.py
@@ -38,5 +38,7 @@ def predict():
     except Exception as e:
         return f"Error: {str(e)}"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#   app.run(debug=True)
+port = int(os.environ.get('PORT', 5000))
+app.run(debug=True, host='0.0.0.0', port=port)
